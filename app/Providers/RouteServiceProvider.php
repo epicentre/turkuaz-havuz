2083624dcb99e0f8616f69c81b5404e59dae3d11
Arif <cwepicentre@gmail.com>
2020-05-09 01:19:42 +0300

    Preparation for spa

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,7 +46,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        //
+        $this->mapSpaRoutes();
     }
 
     /**
@@ -76,5 +76,19 @@ class RouteServiceProvider extends ServiceProvider
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Define the "spa" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapSpaRoutes()
+    {
+        Route::middleware('spa')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/spa.php'));
     }
 }
